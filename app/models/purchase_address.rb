26d2f3f7c6d137ec952a83e97434a1cce45d91c9
fujifujitomo <futomo0421@gmail.com>
2023-07-01 13:29:17 +0900

    購入画面表示

---
+++ app/models/purchase_address.rb
@@ -0,0 +1,20 @@
+class PurchaseAddress
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number,:purchase, :item_id, :user_id
+
+  with_options presence: true do
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: 'is invalid'}
+    validates :user_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  end
+  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+
+  def save
+    # 寄付情報を保存し、変数donationに代入する
+    purchase = Purchase.create(price: price, user_id: user_id)
+    # 住所を保存する
+    # donation_idには、変数donationのidと指定する
+    Address.create(postal_code: postal_code, prefecture: prefecture, city: city, addresses: addresses, building: building, bphone_number: phone_number,purchase_id: purchase.id)
+  end
+
+end
